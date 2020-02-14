d78a16808a632057bd189fcee65186b1c2885fe6
Jake Pino <jakepino@yahoo.com>
2020-02-14 20:02:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,7 +34,7 @@ def map_to_no_change(source_array)
     
   def reduce_to_total(source_array, starting_point)
       total = 0
-      starting = starting_point
+      i = 0
     source_array.each do |ele|
       total += ele
     end
