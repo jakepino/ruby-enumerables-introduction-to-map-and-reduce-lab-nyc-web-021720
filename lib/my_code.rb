a430a5e67d08fdfcb2e7f0668506f798d5579169
Jake Pino <jakepino@yahoo.com>
2020-02-14 20:10:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,24 +23,26 @@ def map_to_no_change(source_array)
    return new_arr
   end
   
-  def map_to_square(source_array)
+def map_to_square(source_array)
     new_arr = []
     source_array.each do |ele|
       new_arr << ele * ele
     end
     return new_arr
     
-    end  
+end  
     
-  def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array, starting_point = 0)
     
     total = starting_point
     source_array.each do |ele|
     total += ele
     end
     return total
-  end
+end
       
+def reduce_to_all_true(source_array)
+    
   
   
   
