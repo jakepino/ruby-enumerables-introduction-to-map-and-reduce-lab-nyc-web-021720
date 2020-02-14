9f7ce7dd0a46982419d911846b6050d9e7e02fd8
Jake Pino <jakepino@yahoo.com>
2020-02-14 20:12:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,8 +42,11 @@ def reduce_to_total(source_array, starting_point = 0)
 end
       
 def reduce_to_all_true(source_array)
-    
+  source_array.each do |ele|
+    ele == true 
+  end
+  return true  
   
-  
+end  
   
   
